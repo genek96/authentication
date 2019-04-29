5e3492aa438872d906d9c338223c268efe5126b6
Evgeniy <genek_96@mail.ru>
2019-04-29 11:25:41 +0300

    integrated with persistance

--- src/authentication.py
+++ src/authentication.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask import request
 import requests as http_request
 from hashlib import sha256
 import json
+import os
 
+ip_adress = os.environ.get("USER_PERSISTENCE_URL") # "http://10.240.20.108:5001"
 PERSISTANCE_API = "/innoreports/user"
 
 app = Flask(__name__)
 
 @app.route('/innoreports/login', methods=['POST', 'GET'])
 def login():
-    response = {'status': None, 'message': None, 'data': {}}   # answer to the client
+    response = {'status': " ", 'message': " ", 'token': " " }   # answer to the client
     if request.method != 'POST':
-        return 'Error request type'
+        return Response (json.dumps(response), status=401)
 
     user_login = str(request.form['login'])
     password = str(request.form['password'])
@@ -23,28 +25,29 @@ def login():
     if len(user_login) < 3 or len(password) < 3:
         response['status'] = 'Error'
         response['message'] = 'Login or password is incorrect!'
-        return json.dumps(response)
+        return Response (json.dumps(response), status=401)
 
     user = __get_user_data(user_login)
     if user is None:
         response['status'] = 'Error'
         response['message'] = "The user with such email doesn't exists!"
-        return json.dumps(response)
+        return Response (json.dumps(response), status=401)
     if user['password'] != password:
         response['status'] = 'Error'
         response['message'] = "The password is incorrect!"
-        return json.dumps(response)
+        return Response (json.dumps(response), status=401)
 
     token = sha256(user_login.encode() + password.encode()).hexdigest()
     if not __update_token(user_login, token):
         response['status'] = 'Error'
         response['message'] = "Some problems occurred during the token obtaining"
-        return json.dumps(response)
+        return Response (json.dumps(response), status=409)
 
     response['status'] = "Success"
     response['message'] = "You have successfully logged in!"
-    response['data']['token'] = token
-    return json.dumps(response)
+    response['token'] = token
+    response['email'] = user_login
+    return Response (json.dumps(response), status=200)
 
 
 def __get_user_data(login):
@@ -53,24 +56,22 @@ def __get_user_data(login):
     :param login: user login to be checked
     :return: user data in success case, else returns None
     """
-    # mock start
-    user = {'email': login, 'password': 'admin'}
-    return user
-    # mock end
-    data = {'login': login}
-    response = http_request.post(url=PERSISTANCE_API + '/getUser', data=data)
-    user = response.json()
-    if user['email'] == "":
-        return None
+    data = {'email': login}
+    response = http_request.post(url=ip_adress + PERSISTANCE_API + '/getUser', data=data)
+    print (response)
+    if response.status_code != 200:
+    	return None
+    user_t = response.text.replace("'",'"')
+    user = json.loads(user_t)
     return user
 
 
 def __update_token(email, token):
     # mock start
-    return True
+    # return True
     # mock end
     data = {'email': email, 'token': token}
-    response = http_request.put(url=PERSISTANCE_API + '/updateToken', data=data).json()
-    if response is None:
-        return False
+    response = http_request.put(url=ip_adress + PERSISTANCE_API + '/updateToken', json=data)
+    if response.status_code != 200:
+    	return False
     return True
