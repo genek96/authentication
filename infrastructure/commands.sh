6b5b5d49678e19fcbdc6b1065a78ec269d678c64
nethmihetti <nethmihettiarachchi484@gmail.com>
2019-04-30 13:49:17 +0300

    dev-ops1

--- infrastructure/commands.sh
+++ infrastructure/commands.sh
@@ -7,7 +7,7 @@ cd authentication
 docker build --no-cache -f infrastructure/Dockerfile -t innoreportsauthentication .
 
 
-docker run -e FLASK_APP='/src/authentication.py' \
+docker run -d -e FLASK_APP='/src/authentication.py' \
  -e LC_ALL='C.UTF-8' \
  -e LANG='C.UTF-8' \
  -e USER_PERSISTENCE_URL=http://10.90.138.222:5001 \
